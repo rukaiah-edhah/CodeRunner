040eaedfece580e8adc2378b7509707fd0e0affb
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-07-20 19:56:23 -0400

    code page design + quiz page modification

--- scripts/wood_box.gd
+++ scripts/wood_box.gd
@@ -5,25 +5,25 @@ var active = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+    pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if $coin != null:
-		$coin.visible = active
-		
-	
-	
-	
-	
-	
-	
-	
-		#$c_style_box.visible = active
+    if $coin != null:
+        $coin.visible = active
+        
+    
+    
+    
+    
+    
+    
+    
+        #$c_style_box.visible = active
 
 
 func _on_body_entered(body):
-	if body.is_in_group("player"):
-		active = true
-		
+    if body.is_in_group("player"):
+        active = true
+        
