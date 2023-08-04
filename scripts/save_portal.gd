fd1ed444372034a3b40a31102686639fecd2068d
Shevan Abdulla <shevan.jabary@gmail.com>
2023-08-04 17:40:33 -0400

    added save_portal scene, script and images

---
+++ scripts/save_portal.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		save_game()
+
+
+func save_game():
+	var save_game = FileAccess.open("res://savegame.save", FileAccess.WRITE_READ)
+	
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for node in save_nodes:
+		#handling exceptions
+		if node.scene_file_path.is_empty(): #skiping nodes that non instanced scenes
+			print("persistent node '%s' is not an instanced scene, skipped" % node.name)
+			continue
+		
+		if !node.has_method("save"): #skiping scenes that do not have a save function
+			print("persistent node '%s' is missing a save() function, skipped" % node.name)
+			continue
+		
+		var node_data = node.call("save") #calling the save function with in the node
+		
+		var json_string = JSON.stringify(node_data,"", true, false) #converting dictionary to JASON
+		
+		save_game.store_line(json_string) #save the dictionary as a new line in the save file
+
