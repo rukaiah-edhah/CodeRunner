4b6a58084d7c43c67244800e6ca7f6416166073d
Shevan Abdulla <shevan.jabary@gmail.com>
2023-08-04 17:52:07 -0400

    added the save_portal to main and created the group Persist on main

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -9,44 +9,44 @@ var _font = preload("res://fonts/quiz_code_pages_font/LibreBaskerville-Regular.t
 var button_bg = preload("res://images/themes/purple_button.tres")
 
 func _ready():
-    # menu option labels
-    start_button.text = "New Game"
-    load_button.text = "Load Game"
-    options_button.text = "Options"
-    quit_button.text = "Quit"
-    
-    start_button.add_theme_font_override("font", _font)
-    load_button.add_theme_font_override("font", _font)
-    options_button.add_theme_font_override("font", _font)
-    quit_button.add_theme_font_override("font", _font)
-    
-    start_button.add_theme_stylebox_override("normal", button_bg)
-    load_button.add_theme_stylebox_override("normal", button_bg)
-    options_button.add_theme_stylebox_override("normal", button_bg)
-    quit_button.add_theme_stylebox_override("normal", button_bg)
-    
-    start_button.add_theme_stylebox_override("hover", button_bg)
-    load_button.add_theme_stylebox_override("hover", button_bg)
-    options_button.add_theme_stylebox_override("hover", button_bg)
-    quit_button.add_theme_stylebox_override("hover", button_bg)
-    
-    start_button.add_theme_stylebox_override("pressed", button_bg)
-    load_button.add_theme_stylebox_override("pressed", button_bg)
-    options_button.add_theme_stylebox_override("pressed", button_bg)
-    quit_button.add_theme_stylebox_override("pressed", button_bg)
+	# menu option labels
+	start_button.text = "New Game"
+	load_button.text = "Load Game"
+	options_button.text = "Options"
+	quit_button.text = "Quit"
+	
+	start_button.add_theme_font_override("font", _font)
+	load_button.add_theme_font_override("font", _font)
+	options_button.add_theme_font_override("font", _font)
+	quit_button.add_theme_font_override("font", _font)
+	
+	start_button.add_theme_stylebox_override("normal", button_bg)
+	load_button.add_theme_stylebox_override("normal", button_bg)
+	options_button.add_theme_stylebox_override("normal", button_bg)
+	quit_button.add_theme_stylebox_override("normal", button_bg)
+	
+	start_button.add_theme_stylebox_override("hover", button_bg)
+	load_button.add_theme_stylebox_override("hover", button_bg)
+	options_button.add_theme_stylebox_override("hover", button_bg)
+	quit_button.add_theme_stylebox_override("hover", button_bg)
+	
+	start_button.add_theme_stylebox_override("pressed", button_bg)
+	load_button.add_theme_stylebox_override("pressed", button_bg)
+	options_button.add_theme_stylebox_override("pressed", button_bg)
+	quit_button.add_theme_stylebox_override("pressed", button_bg)
 
 func _on_start_button_pressed():
-    # when you press start -> go to game
-    get_tree().change_scene_to_file("res://scenes/main.tscn")
+	# when you press start -> go to game
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_options_button_pressed():
-    # when you press options -> go to options menu
-    get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
+	# when you press options -> go to options menu
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 func _on_quit_button_pressed():
-    # when you press quit -> leave game 
-    get_tree().quit()
+	# when you press quit -> leave game 
+	get_tree().quit()
 
 func _on_load_button_pressed():
-    # when you press load game (must be filled later when shevan is done with save functions)
-    pass # Replace with function body.
+	# when you press load game (must be filled later when shevan is done with save functions)
+	pass # Replace with function body.
