f14db5533ca4d99f5874ec80e6716acf0c02b3cb
Shevan Abdulla <shevan.jabary@gmail.com>
2023-08-04 18:29:00 -0400

    added the load function to the load button on menu and tested it

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -48,5 +48,39 @@ func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _on_load_button_pressed():
-	# when you press load game (must be filled later when shevan is done with save functions)
-	pass # Replace with function body.
+	#Handling Exceptions
+	if not FileAccess.file_exists("res://savegame.save"): #no save file
+		print("Error! We don't have a save to load.")
+	
+	#delete the data in the save file
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		i.queue_free()
+		
+	var save_game = FileAccess.open("res://savegame.save", FileAccess.READ)
+	while save_game.get_position() < save_game.get_length():
+		var json_string = save_game.get_line()
+
+		# Creates the helper class to interact with JSON
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		#Get the data from the JSON object
+		var node_data = json.get_data()
+
+		#Firstly, we need to create the object and add it to the tree and set its position.
+		var new_object = load(node_data["filename"]).instantiate()
+		get_node(node_data["parent"]).add_child(new_object)
+		new_object.position = Vector2(node_data["pos_x"], node_data["pos_y"])
+
+		#Now we set the remaining variables.
+		for i in node_data.keys():
+			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+				continue
+			new_object.set(i, node_data[i])
+
