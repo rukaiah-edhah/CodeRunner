08401fb9441b86658ac12f6fe870486dd89e6962
Abutalibhasan <66400651+Abutalibhasan@users.noreply.github.com>
2023-08-07 01:23:43 -0500

    implemented reading from text file feature

--- singleton/static_data.gd
+++ singleton/static_data.gd
@@ -1,27 +1,21 @@
 extends Node
 
 var item_data = []
-var files_arr = []
 
-var data_file_path = "res://Data/data.txt"
+
+var data_file_path = "res://materials/data.txt"
 
 func _ready():
 	item_data = load_json_file(data_file_path)
 
 func load_json_file(file_path: String):
 	if FileAccess.file_exists(file_path):
-		var data_file = FileAccess.open(file_path,FileAccess.READ)
-		#return data_file
-		#var parsed_result = Array#JSON.parse_string(data_file.get_as_text())
-		#if parsed_result is Dictionary:
-		var x = data_file.get_as_text()
-		files_arr.append(x)
-		return files_arr
-#		else:
-#			print("Error loading the file")
-#
-#	else:
-#		print("file doesn't exist")
+		var data_file = FileAccess.open(file_path,FileAccess.READ) #return data_file
 		
+		var str_text = data_file.get_as_text()
+		item_data.append(str_text)
+		#print(item_data)
+		return item_data
+#		
 
 	
