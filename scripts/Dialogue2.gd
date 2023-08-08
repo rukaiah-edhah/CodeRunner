4b7d52c4c9d8b8aa61c39d378c4ad2da6f88d59c
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-08-08 14:09:17 -0400

    Worked more on building the playable area for the Python level and covering it with graphics + Fixed the scroll issue, added a block area, and changed the sprite for the dialogue boxe

--- scripts/Dialogue2.gd
+++ scripts/Dialogue2.gd
@@ -1,14 +1,14 @@
 extends Control
 
-var messages
-#	'Python Comments:',
-#	'Comments can be used to explain Python code.
-#
-#Comments can be used to make the code more readable.
-#
-#Comments can be used to prevent execution when testing code.' ,
-#'#This is a comment'
-#	]
+var messages = [
+    'Python Comments:',
+    'Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code.' ,
+'#This is a comment'
+    ]
 
 var typing_speed = .2
 var read_time = 2
@@ -21,25 +21,21 @@ var active = false
 
 
 func _ready():
-    messages = StaticData.item_data
-    print(messages)
     start_dialogue()
     
-    
-    
 
-func _process(_delta):
+func _process(delta):
     if Input.is_action_just_pressed("ui_accept"):
         #start_dialogue()
         stop_dialogue()
-
+    
 func start_dialogue():
     current_message = 0
     display = ""
     current_char = 0
     $Arrow.visible = active
-
-
+    
+    
     $next_char.set_wait_time(typing_speed)
     $next_char.start()
 
@@ -53,7 +49,7 @@ func _on_next_char_timeout():
     if (current_char < len(messages[current_message])):
         var next_char = messages[current_message][current_char]
         display += next_char
-
+        
         $RichTextLabel.text = display
         current_char += 1
     else:
