e758d09f4ab06c4c24e141371a289ed22f51b27e
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-08-10 00:27:59 -0400

    Made the background in the intro level follows the player, fixed some issues with the paralllax effect between the graphcis and the in game items, and built a structure for our integration process for both the intro and the python level

--- scripts/Dialogue.gd
+++ scripts/Dialogue.gd
@@ -33,76 +33,76 @@ var is_dissolving_window = false
 
 
 func _ready():
-	block_area.player_hit.connect(_on_block_area_player_hit)
-	anim_player.animation_finished.connect(_on_animation_player_animation_finished)
-	
-	messages = StaticData.item_data
-	print(messages)
-	start_dialogue()
-	
-	dialogue_text.scroll_active = false # Disable manual scrolling
-	
+    block_area.player_hit.connect(_on_block_area_player_hit)
+    anim_player.animation_finished.connect(_on_animation_player_animation_finished)
+    
+    messages = StaticData.item_data
+    print(messages)
+    start_dialogue()
+    
+    dialogue_text.scroll_active = false # Disable manual scrolling
+    
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		#start_dialogue()
-		stop_dialogue()
+    if Input.is_action_just_pressed("ui_accept"):
+        #start_dialogue()
+        stop_dialogue()
 
 func start_dialogue():
-	#anim_player.play("display")
-	current_message = 0
-	display = ""
-	current_char = 0
-	$dialogue_box_sprite/panel/Arrow.visible = active
+    #anim_player.play("display")
+    current_message = 0
+    display = ""
+    current_char = 0
+    $dialogue_box_sprite/panel/Arrow.visible = active
 
 
-	$next_char.set_wait_time(typing_speed)
-	$next_char.start()
+    $next_char.set_wait_time(typing_speed)
+    $next_char.start()
 
 func stop_dialogue():
-	# get_parent().remove_child(self)
-	if (current_message == len(messages) - 1):
-		anim_player.play("dissolve")
-		queue_free()
-		block_area.queue_free()
+    # get_parent().remove_child(self)
+    if (current_message == len(messages) - 1):
+        anim_player.play("dissolve")
+        queue_free()
+        block_area.queue_free()
 
 func _on_next_char_timeout():
-	if (current_char < len(messages[current_message])):
-		var next_char = messages[current_message][current_char]
-		display += next_char
+    if (current_char < len(messages[current_message])):
+        var next_char = messages[current_message][current_char]
+        display += next_char
 
-		dialogue_text.text = display
-		dialogue_text.scroll_to_line(dialogue_text.get_line_count() - 1) # Automatically scroll to the end
-		
-		current_char += 1
-	else:
-		$next_char.stop()
-		$next_message.one_shot = true
-		$next_message.set_wait_time(read_time)
-		$next_message.start()
+        dialogue_text.text = display
+        dialogue_text.scroll_to_line(dialogue_text.get_line_count() - 1) # Automatically scroll to the end
+        
+        current_char += 1
+    else:
+        $next_char.stop()
+        $next_message.one_shot = true
+        $next_message.set_wait_time(read_time)
+        $next_message.start()
 
 func _on_next_message_timeout():
-	if (current_message == len(messages) - 1):
-		$dialogue_box_sprite/panel/Arrow.visible = true
-		if Input.is_action_just_pressed("ui_accept"):
-			stop_dialogue()
-	else: 
-		current_message += 1
-		display = ""
-		current_char = 0
-		$next_char.start()
+    if (current_message == len(messages) - 1):
+        $dialogue_box_sprite/panel/Arrow.visible = true
+        if Input.is_action_just_pressed("ui_accept"):
+            stop_dialogue()
+    else: 
+        current_message += 1
+        display = ""
+        current_char = 0
+        $next_char.start()
 
 #_____________________________Added by Rukaiah__________________________
 func _on_block_area_player_hit():
-	if is_dissolving_window or is_displaying_window:
-		return
+    if is_dissolving_window or is_displaying_window:
+        return
 
-	anim_player.play("shake_animation")
-	
+    anim_player.play("shake_animation")
+    
 func _on_animation_player_animation_finished(anim_name):
-	match anim_name:
-		"display":
-			is_displaying_window = false
-		"dissolve":
-			is_dissolving_window = false
+    match anim_name:
+        "display":
+            is_displaying_window = false
+        "dissolve":
+            is_dissolving_window = false
 #________________________________________________________________________
