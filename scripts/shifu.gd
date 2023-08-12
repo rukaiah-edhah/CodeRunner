74e9dfc419768eb5db10131b68a2fd436b52f612
Shevan Abdulla <shevan.jabary@gmail.com>
2023-08-11 21:02:55 -0400

    added sleep and wake up animaiton to shifu and connected to the player entering or exiting the encounter area

--- scripts/shifu.gd
+++ scripts/shifu.gd
@@ -1,18 +1,24 @@
 extends Node2D
 
-@onready var encounter_area = $Sprite2D/encounter_area
+@onready var encounter_area = $AnimatedSprite2D/encounter_area
 @onready var dialogue_box = $dialogue_box
-@onready var animation = $Sprite2D
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _ready():
-    encounter_area.body_entered.connect(_on_shifu_encounter_area_body_entered)
-    dialogue_box.visible = false
+	_animated_sprite.play("sleep")
+	encounter_area.body_entered.connect(_on_shifu_encounter_area_body_entered)
+	encounter_area.body_exited.connect(_on_shifu_encounter_area_body_exited)
 
+	dialogue_box.visible = false
+
+	
 func _on_shifu_encounter_area_body_entered(body):
-    if body.is_in_group("player"):
-        # CHANGE SHIFU'S POSITION: animation.play("name_of_the_animation")
-        # Add anything else you want before the dialougue box appear here 
-        dialogue_box.visible = true
-        
-        
-# Note: After the dialogue box scene has ended, try to make it appear again if the player collided with Shifu again
+	if body.is_in_group("player"):
+		_animated_sprite.play_backwards("wakeup")
+		dialogue_box.visible = true
+
+		
+func _on_shifu_encounter_area_body_exited(body):
+	if body.is_in_group("player"):
+		_animated_sprite.play("wakeup")
+		dialogue_box.visible = false
