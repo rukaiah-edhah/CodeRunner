268e1b058f99d201b3834e9858719942daa8c072
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-08-14 19:30:41 -0400

    Made the dialogue panel a bit darker

--- scripts/shifu.gd
+++ scripts/shifu.gd
@@ -7,26 +7,26 @@ extends Node2D
 
 
 func _ready():
-	
-	encounter_area.body_entered.connect(_on_shifu_encounter_area_body_entered)
-	encounter_area.body_exited.connect(_on_shifu_encounter_area_body_exited)
+    
+    encounter_area.body_entered.connect(_on_shifu_encounter_area_body_entered)
+    encounter_area.body_exited.connect(_on_shifu_encounter_area_body_exited)
 
-	dialogue_box.visible = false
-	
+    dialogue_box.visible = false
+    
 func _on_shifu_encounter_area_body_entered(body):
-	if body.is_in_group("player"):
-		dialogue_box.visible = true
-		dialogue_box.reset_scene()
-		_animated_sprite.play("wake_up")
-		await _animated_sprite.animation_finished
-		_animated_sprite.play("idle")
+    if body.is_in_group("player"):
+        dialogue_box.visible = true
+        dialogue_box.reset_scene()
+        _animated_sprite.play("wake_up")
+        await _animated_sprite.animation_finished
+        _animated_sprite.play("idle")
 
-	
+    
 func _on_shifu_encounter_area_body_exited(body):
-	if body.is_in_group("player"):
-		_animated_sprite.play("back_to_sleep")
-		$dialogue/AnimationPlayer.play("dissolve")
-		#dialogue_box.visible = false  
+    if body.is_in_group("player"):
+        _animated_sprite.play("back_to_sleep")
+        $dialogue/AnimationPlayer.play("dissolve")
+        #dialogue_box.visible = false  
 
 
 
