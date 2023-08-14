a83a7f5674534e48c7b3a19a0ab013dcb5bcb64d
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-08-14 14:23:21 -0400

    Restarted the dialogue scene when the player leaves the encounter area of shifu, fixed some animations with shifu, and fixed the queue_free() error in the block area.

--- scripts/shifu.gd
+++ scripts/shifu.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var encounter_area = $AnimatedSprite2D/encounter_area
-@onready var dialogue_box = $dialogue_box
+@onready var dialogue_box = $dialogue
 @onready var _animated_sprite = $AnimatedSprite2D
 
+
+
 func _ready():
+    
     encounter_area.body_entered.connect(_on_shifu_encounter_area_body_entered)
     encounter_area.body_exited.connect(_on_shifu_encounter_area_body_exited)
 
@@ -12,10 +15,18 @@ func _ready():
     
 func _on_shifu_encounter_area_body_entered(body):
     if body.is_in_group("player"):
-        _animated_sprite.play_backwards("wakeup")
         dialogue_box.visible = true
+        dialogue_box.reset_scene()
+        _animated_sprite.play("wake_up")
+        await _animated_sprite.animation_finished
+        _animated_sprite.play("idle")
+
     
 func _on_shifu_encounter_area_body_exited(body):
     if body.is_in_group("player"):
-        _animated_sprite.play("wakeup")
-        dialogue_box.visible = false
+        _animated_sprite.play("back_to_sleep")
+        $dialogue/AnimationPlayer.play("dissolve")
+        #dialogue_box.visible = false  
+
+
+
