53d214dd3878cbc02c8146534b1959171de6cef0
izabellelantz <izabellemlantz@gmail.com>
2023-08-14 19:58:38 -0500

    Sound effect for picking up coins added

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,7 +3,15 @@ extends Area2D
 # signal to be sent to player to track coins picked up
 signal coin_inventory_changed
 
+@onready var coin_sound = $coin_audio
+
+var audio = preload("res://fonts-and-music/music/pickupCoin.wav")
+
+func _ready():
+	coin_sound.stream = audio
+
 func _on_body_entered(body):
+	coin_sound.play()
 	# send signal to player and make coin dissappear
 	if body.is_in_group("player"):
 		emit_signal("coin_inventory_changed")
