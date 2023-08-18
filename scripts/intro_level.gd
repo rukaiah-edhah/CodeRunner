c073a76b3794a3e33be843f3c1c6d74fbe2554a5
Abutalibhasan <66400651+Abutalibhasan@users.noreply.github.com>
2023-08-18 16:40:46 -0500

    added and commited the remote changes

--- scripts/intro_level.gd
+++ scripts/intro_level.gd
@@ -3,13 +3,13 @@ extends Node2D
 const music_path: String = "res://fonts-and-music/music/DavidKBD - Cosmic Pack 01 - Cosmic Journey-variation2.ogg"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $bg_music.stream = preload(music_path)
-    $bg_music.volume_db = -15
-    $bg_music.play()
-    
+	$bg_music.stream = preload(music_path)
+	$bg_music.volume_db = -15
+	$bg_music.play()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
