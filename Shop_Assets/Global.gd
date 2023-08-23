0a261ee3afafd0bc50fd120876660844ee59ddc7
rukaiah-edhah <rukaiah.edhah@yahoo.com>
2023-08-23 17:27:21 -0400

    Enabled the global script that was used in the shop

--- Shop_Assets/Global.gd
+++ Shop_Assets/Global.gd
@@ -3,46 +3,46 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 var gold = 100000
 var item  = {
-	1: {
-		"Name": "Apple",
-		"Desc": "This is an apple.",
-		"cost": 10
-	},
-	2: {
-		"Name": "Berry",
-		"Desc": "Great in yogurt",
-		"cost": 2
-	},
-	3: {
-		"Name": "Carrot",
-		"Desc": "Whats up Doc?",
-		"cost": 5
-	}
+    1: {
+        "Name": "Apple",
+        "Desc": "This is an apple.",
+        "cost": 10
+    },
+    2: {
+        "Name": "Berry",
+        "Desc": "Great in yogurt",
+        "cost": 2
+    },
+    3: {
+        "Name": "Carrot",
+        "Desc": "Whats up Doc?",
+        "cost": 5
+    }
 }
 
 
 var inventory = {
-	0: {
-		"Name": "Apple",
-		"Desc": "This is an apple.",
-		"cost": 10,
-		"Count": 1,
-		"Item_Icon": preload("res://images/Berry.png")
-	},
-	1: {
-		"Name": "Berry",
-		"Desc": "Great in yogurt",
-		"cost": 2,
-		"Count": 0
-	}
+    0: {
+        "Name": "Apple",
+        "Desc": "This is an apple.",
+        "cost": 10,
+        "Count": 1,
+        #"Item_Icon": preload("res://images/Berry.png")
+    },
+    1: {
+        "Name": "Berry",
+        "Desc": "Great in yogurt",
+        "cost": 2,
+        "Count": 0
+    }
 }
--- Shop_Assets/item_slot.gd
+++ Shop_Assets/item_slot.gd
@@ -10,42 +10,42 @@ var mouseEntered = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if hasitem == true:
-		get_node("Item_Icon").show()
-		get_node("Count").show()
-	else:
-		get_node("Item_Icon").hide()
-		get_node("Count").hide()
-		
-		
-		
+    if hasitem == true:
+        get_node("Item_Icon").show()
+        get_node("Count").show()
+    else:
+        get_node("Item_Icon").hide()
+        get_node("Count").hide()
+        
+        
+        
 func _input(event):
-	if event.is_action("Left_Click"):
-		if mouseEntered:
-			get_node("res://Shop_Assets/Item_Info/Icon")
+    if event.is_action("Left_Click"):
+        if mouseEntered:
+            get_node("res://Shop_Assets/Item_Info/Icon")
 
 
 func _on_mouse_entered():
-	if hasitem == true:
-		mouseEntered= true
+    if hasitem == true:
+        mouseEntered= true
 
 
 func _on_mouse_exited():
-	mouseEntered = false
+    mouseEntered = false
 
 func _input2(event):
-	if event.is_action_pressed("Left_Click"):
-		if mouseEntered:
-			get_node("Item_Info/Icon").texture = get_node("Icon")
-			get_node("item_info").ItemName = Item_Name
-			get_node("item_info").ItemDes = Item_Des
-			get_node("item_info").ItemCost = Item_Cost
-			get_node("item_info").ItemCount = Item_Count
-			get_node("Item_Info_AP").play("Trans_In")
-			get_node("/root/ItemInfo").UpdateInfo()
+    if event.is_action_pressed("Left_Click"):
+        if mouseEntered:
+            get_node("Item_Info/Icon").texture = get_node("Icon")
+            get_node("item_info").ItemName = Item_Name
+            get_node("item_info").ItemDes = Item_Des
+            get_node("item_info").ItemCost = Item_Cost
+            get_node("item_info").ItemCount = Item_Count
+            get_node("Item_Info_AP").play("Trans_In")
+            get_node("/root/ItemInfo").UpdateInfo()
 
